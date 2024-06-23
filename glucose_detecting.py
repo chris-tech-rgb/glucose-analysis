8394f53794b4003e6ddf360ac37a4dcb2b101bb9
cyl <afgugewufv@gmail.com>
2024-06-23 20:26:11 +0900

    excel

--- glucose_detecting.py
+++ glucose_detecting.py
@@ -92,7 +92,7 @@ def concentration(imgs):
   # Export as excel
   df = pd.DataFrame([[a] + [float("{:.2f}".format(b))] + c.tolist() for a, b, c in zip(measured, pH, rgb)],
   columns=['Glucose concentration (mM, measured)', 'Glucose concentration (mM, predicted)', 'R', 'G', 'B'])
-  df.to_excel("glucose concentration predicting.xlsx", index=False)
+  df.to_excel("excel/glucose concentration predicting.xlsx", index=False)
 
 def main():
   image_dict = load_images('training data')
